f5ced810c7d94c0b9f7fe58192fd4a0b990861a7
Kevin Dang <dangkevin12@gmail.com>
2025-07-27 19:53:37 -0400

    simplify atoms

--- src/cli/ui/atoms/game-atom.ts
+++ src/cli/ui/atoms/game-atom.ts
@@ -1,23 +1,11 @@
 import { atom } from 'jotai';
 import { WordleGame } from '../../../core/game';
 
-// Writable atom to hold the game instance
-export const gameAtom = atom<WordleGame>(new WordleGame());
+const game = new WordleGame();
 
-// Derived atom to get the game state
-export const gameStateAtom = atom((get) => get(gameAtom).getState());
+export const gameStateAtom = atom(game.getState());
 
-// Write-only atom to make a guess and update the game instance
-export const makeGuessAtom = atom(null, (get, set, guess: string) => {
-  const game = get(gameAtom);
+export const makeGuessAtom = atom(null, (_get, set, guess: string) => {
   game.makeGuess(guess);
-  // Force update by creating a new instance if needed, or just set to same instance
-  set(gameAtom, Object.assign(Object.create(Object.getPrototypeOf(game)), game));
-});
-
-// Write-only atom to reset the game
-export const resetGameAtom = atom(null, (get, set) => {
-  const game = get(gameAtom);
-  game.reset();
-  set(gameAtom, game);
+  set(gameStateAtom, game.getState());
 });
