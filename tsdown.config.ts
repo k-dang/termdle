497d4ada5ba8f9d346b786694b4a4c0e837b7b87
Kevin Dang <dangkevin12@gmail.com>
2025-08-16 12:51:15 -0400

    chore: update deps again

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,7 +3,5 @@ import { defineConfig } from 'tsdown';
 export default defineConfig({
   entry: ['./src/cli/index.tsx'],
   platform: 'node',
-  dts: {
-    isolatedDeclarations: true,
-  },
+  minify: true
 });
